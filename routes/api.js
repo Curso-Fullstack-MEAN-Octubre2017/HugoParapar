439680fb1a59a6f58ca95d30293963de943ed792
Hugo <JAVA@PC07A3>
2017-10-17 14:08:49 +0200

    Task: #24: Rest :id

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var CustomerController = require('../controller/customer.js');
 var PetsController = require('../controller/pets.js');
+var AppoinmentController = require('../controller/appoinment.js');
 var api = express.Router();
 
 //GET
@@ -8,14 +9,18 @@ api.get('/customers',CustomerController.getCustomers);
 api.get('/customers/:id',CustomerController.getCustomerById);
 api.get('/customers/:id/pets',PetsController.getPetsByCustomerId);
 api.get('/pets/:id',PetsController.getPetsById);
+api.get('/appointments',AppoinmentController.getAppoinments);
+api.get('/appointments/:id',AppoinmentController.getAppoinmentById);
 
 //POST
 api.post('/customers',CustomerController.postCustomer);
 api.post('/pets',PetsController.postPets);
+api.post('/appointments',AppoinmentController.postAppointment);
 
 //PUT
 api.put('/customers/:id',CustomerController.updateCustomer);
 api.put('/pets/:id',PetsController.updatePet);
+api.put('/appointments/:id',AppoinmentController.updateAppointment);
 
 //DELETE
 api.delete('/pets/:id',PetsController.deletePet);
